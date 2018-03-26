0a8097aa8eef6cf795ac9a2dcd8592c482b23341
HUGHBRADY <G00338260@gmit.ie>
2018-03-26 17:55:43 +0100

    Shunting algorithm completed
    Regexps are now being converted from infix to postfix.

--- shunt.go
+++ shunt.go
@@ -5,9 +5,43 @@ import (
 )
 
 func intopost(infix string) string {
+	// Maps runes to ints. These are ordered by precedence in regexps
 	specials := map[rune]int{'*': 10, '.': 9, '|': 8}
 
 	postfix, s := []rune{}, []rune{}
+
+	// Loop over infix string a char at a time
+	for _, r := range infix {
+		switch {
+		case r == '(':
+			// Throws open bracket onto s stack
+			s = append(s, r)
+		case r == ')':
+			// Loop through stack until you see open bracket
+			for s[len(s)-1] != '(' {
+				// Keep popping chars off stack and appending to postfix
+				postfix, s = append(postfix, s[len(s)-1]), s[:len(s)-1]
+			}
+			// Finally, discard open bracket
+			s = s[:len(s)-1]
+		case specials[r] > 0:
+			// while stack has elements, and precedence of the character <= the precedence of last element
+			for len(s) > 0 && specials [r] <= specials[s[len(s)-1]] {
+				// Pop the elements off top of stack and append to postfix
+				postfix, s = append(postfix, s[len(s)-1]), s[:len(s)-1]
+			}
+			s = append(s, r)
+		// Normal characters eg a, b, c
+		default:
+			// Appends r to the end of the postfix[]
+			postfix = append(postfix, r)
+		}
+	}
+
+	// If anything is left in the stack pop it into postfix 
+	for len(s) > 0 {
+		postfix, s = append(postfix, s[len(s)-1]), s[:len(s)-1]
+	}
 
 	return string(postfix)
 }
@@ -18,8 +52,8 @@ func main() {
 	fmt.Println("Postfix: 	", intopost("a.b.c*"))
 
 	// Answer abd|.*
-	fmt.Println("Infix:		", "(a.(b|d).c*")
-	fmt.Println("Postfix: 	", intopost("(a.(b|d).c*"))
+	fmt.Println("Infix:		", "(a.(b|d))")
+	fmt.Println("Postfix: 	", intopost("(a.(b|d))"))
 
 	// Answer abd|.c*.
 	fmt.Println("Infix:		", "a.(b|d).c*")
