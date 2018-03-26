d38c1b073f089b50f01db07f3802e5144f752228
HUGHBRADY <G00338260@gmit.ie>
2018-03-26 21:36:32 +0100

    Working on Thompson's construction
    Just set up structs and switches.

--- nfa.go
+++ nfa.go
@@ -4,3 +4,39 @@ import (
 	"fmt"
 )
 
+type state struct {
+	symbol rune 
+	edge1 *state
+	edge2 *state
+}
+
+type nfafrag struct {
+	initial *state
+	accept  *state
+}
+
+func poregtonfa(postfix string) *nfa {
+	// Provides an array of pointers to nfa (struct above) that is empty
+	nfastack := []*nfa{}	
+	
+		for _, r := pofix {
+			switch r {
+			case '.':
+				
+			case '|':
+
+			case '*':
+
+			default:
+
+			}
+		}
+
+	// DO ERROR CHECKING TO MAKE SURE THERE'S ONLY ONE ELEMENT ON NFA STACK
+	return nfastack[0]
+}
+
+func main() {
+	nfa := poregtonfa("ab.c*|")
+	fmt.Println(nfa)
+}
