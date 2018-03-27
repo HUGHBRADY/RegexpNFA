68f83dc0031fdfcde31c461517da8cf4d4a7f1b6
HUGHBRADY <G00338260@gmit.ie>
2018-03-27 16:15:39 +0100

    Thompson's Construction done

--- nfa.go
+++ nfa.go
@@ -10,7 +10,7 @@ type state struct {
 	edge2 *state
 }
 
-type nfafrag struct {
+type nfa struct {
 	initial *state
 	accept  *state
 }
@@ -19,7 +19,7 @@ func poregtonfa(postfix string) *nfa {
 	// Provides an array of pointers to nfa (struct above) that is empty
 	nfastack := []*nfa{}	
 	
-		for _, r := pofix {
+		for _, r := range postfix {
 			switch r {
 			case '.':
 				// Take two elements off nfa stack
@@ -48,12 +48,26 @@ func poregtonfa(postfix string) *nfa {
 				initial := state{edge1: frag1.initial, edge2: frag2.initial}
 				frag1.accept.edge1 = &accept
 				frag2.accept.edge1 = &accept
-				
+				 
 				nfastack = append(nfastack, &nfa{initial: &initial, accept: &accept})
 			case '*':
+				// Pop a fragment off the stack
+				frag := nfastack[len(nfastack)-1]
+				nfastack = nfastack[:len(nfastack)-1]
 
+				accept := state{}
+				initial := state{edge1: frag.initial, edge2: &accept}
+				// New fragment is old frag with 2 extra states (new initial and accept)
+				frag.accept.edge1 = frag.initial
+				frag.accept.edge2 = &accept
+
+				// Push new fragment to nfa stack
+				nfastack = append(nfastack, &nfa{initial: &initial, accept: &accept})
 			default:
+				accept := state{}
+				initial := state{symbol: r, edge1: &accept}
 
+				nfastack = append(nfastack, &nfa{initial: &initial, accept: &accept})
 			}
 		}
 
